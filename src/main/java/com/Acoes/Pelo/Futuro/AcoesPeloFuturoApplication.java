30b530caa7245e23e8465fc6937a47d09535453c
correa-tech <guilhermeluis899@gmail.com>
2025-04-15 13:22:41 -0300

    first commit

---
+++ src/main/java/com/Acoes/Pelo/Futuro/AcoesPeloFuturoApplication.java
@@ -0,0 +1,13 @@
+package com.Acoes.Pelo.Futuro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcoesPeloFuturoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcoesPeloFuturoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Acoes/Pelo/Futuro/AcoesPeloFuturoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Acoes.Pelo.Futuro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcoesPeloFuturoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
